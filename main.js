bd855fb5ba70847b5e90d1c80476701d780cf356
ElZexo <169349249+ElZexo@users.noreply.github.com>
2025-05-06 12:02:09 +0200

    Add the computer choice function
    The new function converts a random number in either "rock", "paper" or
    "scissors" and, at the time being, prints the result in the console.

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 console.log("This is the first Rock Paper Scissors log")
+
+function getComputerChoice() {
+    let moveValue = Math.random();
+    console.log(moveValue)
+    if (moveValue <= 0.33) {
+        return "rock"
+    }
+    else if (moveValue <= 0.67) {
+        return "paper"
+    }
+    else {
+        return "scissors"
+    }
+}
+
+console.log(getComputerChoice())
