1611526bc2ea40ed703194fc499512b8374c154d
ElZexo <169349249+ElZexo@users.noreply.github.com>
2025-05-06 12:45:37 +0200

    Add the function to play a round of the game
    The function reads both the player and computer's inputs, compares them,
    and declares a winner.

--- main.js
+++ main.js
@@ -1,30 +1,61 @@
 console.log("This is the first Rock Paper Scissors log")
 
+let humanScore = 0;
+let computerScore = 0;
+
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection)
+
 function getComputerChoice() {
+    let computerMove = 0;
     let moveValue = Math.random();
-    console.log(moveValue)
     if (moveValue <= 0.33) {
-        return "rock"
+        computerMove = "rock";
     }
     else if (moveValue <= 0.67) {
-        return "paper"
+        computerMove = "paper";
     }
     else {
-        return "scissors"
+        computerMove = "scissors";
     }
+    return computerMove;
 }
 
 function getHumanChoice() {
     let userMove = prompt("Choose your move!").toLowerCase()
     if (userMove == "rock" || userMove == "paper" || userMove == "scissors") {
-        return userMove
+        return userMove;
     }
     else {
-        console.log("Invalid move, please choose again.")
-        getHumanChoice()
+        alert("Invalid move, please choose again.");
+        getHumanChoice();
     }
 }
 
-getHumanChoice()
+function playRound (humanChoice, computerChoice) {
+    console.log("GAME")
+    console.log(humanChoice)
+    console.log(computerChoice)
+    if(humanChoice == "rock" && computerChoice == "scissors"){
+        console.log("You win! Rock beats Scissors.")
+    }
+    else if(humanChoice == "paper" && computerChoice == "rock") {
+        console.log("You win! Paper beats Rock.")
+    }
+    else if(humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("You win! Scissors beats Paper.")
+    }
+    else if(humanChoice == "rock" && computerChoice == "paper") {
+        console.log("You lose! Paper beats Rock.")
+    }
+    else if(humanChoice == "paper" && computerChoice == "scissors") {
+        console.log("You lose! Scissors beats Paper.")
+    }
+    else if(humanChoice == "scissors" && computerChoice == "rock") {
+        console.log("You lose! Rock beats scissors.")
+    }
+    else console.log("That's a tie!")
+}
 
-console.log(getComputerChoice())
